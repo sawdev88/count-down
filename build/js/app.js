37879a870820f6072cefcd889e209616ccb5fba9
Sean Wines <seanwinesdev@gmail.com>
2016-08-23 21:37:57 -0700

    Game logic created. Count down from 30s, add point if sum is correct and change question if answer is wrong/right

--- build/js/app.js
+++ build/js/app.js
@@ -1,13 +1,15 @@
 'use strict';
 
-var randomGenerator = function randomGenerator() {
-  return Math.floor(Math.random() * 100);
+var score = 0;
+
+var randomGenerator = function randomGenerator(num) {
+  return Math.floor(Math.random() * num);
 };
 
 var problem = function problem() {
   var arr = [],
-      ran1 = randomGenerator(),
-      ran2 = randomGenerator();
+      ran1 = randomGenerator(100),
+      ran2 = randomGenerator(100);
 
   arr[0] = ran1;
   arr[1] = ran2;
@@ -15,11 +17,59 @@ var problem = function problem() {
   return arr;
 };
 
-var solution = problem().reduce(function (total, num) {
-  return total + num;
-});
+var shuffle = function shuffle(arr) {
+  for (var i = arr.length - 1; i >= 0; i--) {
 
-console.log(solution);
+    var randomIndex = Math.floor(Math.random() * (i + 1));
+    var itemAtIndex = arr[randomIndex];
+
+    arr[randomIndex] = arr[i];
+    arr[i] = itemAtIndex;
+  }
+  return arr;
+};
+
+var showProblemGetAnswer = function () {
+  var sum = void 0,
+      arr = void 0,
+      btn = void 0,
+      shuffled = void 0;
+
+  var resetGame = function resetGame() {
+    arr = problem();
+
+    arr.reduce(function (total, num) {
+      sum = total + num;
+    });
+
+    $('.problem').text(sum);
+
+    btn = [arr, problem(), problem(), problem()];
+    shuffled = shuffle(btn);
+
+    $('.button1').text(shuffled[0].join(' + ')).val(shuffled[0]);
+    $('.button2').text(shuffled[1].join(' + ')).val(shuffled[1]);
+    $('.button3').text(shuffled[2].join(' + ')).val(shuffled[2]);
+    $('.button4').text(shuffled[3].join(' + ')).val(shuffled[3]);
+  };
+
+  resetGame();
+
+  $('button').click(function () {
+    var choice = $(this).val(),
+        compare = arr.join();
+
+    if (choice === compare) {
+      resetGame();
+      score += 1;
+      $('.score').text(score);
+    } else {
+      resetGame();
+    }
+  });
+}();
+
+//showProblemGetAnswer(problem());
 
 var gameTimer = function gameTimer(time) {
   if (time >= 0) {
@@ -28,6 +78,8 @@ var gameTimer = function gameTimer(time) {
       time -= 1;
       gameTimer(time);
     }, 1000);
+  } else {
+    $('#buttons button').attr("disabled", true);
   }
 };
 
--- src/app.js
+++ src/app.js
@@ -1,32 +1,84 @@
-var randomGenerator = function () {
-  return Math.floor(Math.random() * 100);
+let score = 0;
+
+var randomGenerator = function (num) {
+  return Math.floor(Math.random() * num);
 };
 
 var problem = function () {
   let arr = [],
-      ran1 = randomGenerator(),
-      ran2 = randomGenerator();
+      ran1 = randomGenerator(100),
+      ran2 = randomGenerator(100);
 
   arr[0] = ran1;
   arr[1] = ran2;
 
   return arr;
-
 };
 
-var solution = problem().reduce(function (total, num) {
-  return total + num;
-});
+var shuffle = (arr) => {
+  for (var i = arr.length-1; i >=0; i--) {
 
-console.log(solution);
+      var randomIndex = Math.floor(Math.random()*(i+1));
+      var itemAtIndex = arr[randomIndex];
 
-let gameTimer = function (time) {
+      arr[randomIndex] = arr[i];
+      arr[i] = itemAtIndex;
+  }
+  return arr;
+};
+
+var showProblemGetAnswer = function () {
+  let sum,
+      arr,
+      btn,
+      shuffled;
+
+  var resetGame = function () {
+    arr = problem();
+
+    arr.reduce((total, num) => {
+      sum = total + num;
+    });
+
+    $('.problem').text(sum);
+
+    btn = [arr, problem(), problem(), problem()];
+    shuffled = shuffle(btn);
+
+    $('.button1').text(shuffled[0].join(' + ')).val(shuffled[0]);
+    $('.button2').text(shuffled[1].join(' + ')).val(shuffled[1]);
+    $('.button3').text(shuffled[2].join(' + ')).val(shuffled[2]);
+    $('.button4').text(shuffled[3].join(' + ')).val(shuffled[3]);
+  };
+
+  resetGame();
+
+  $('button').click(function(){
+    let choice = $(this).val(),
+        compare = arr.join();
+
+    if (choice === compare) {
+      resetGame();
+      score += 1;
+      $('.score').text(score);
+    } else {
+      resetGame();
+    }
+  });
+
+}();
+
+//showProblemGetAnswer(problem());
+
+var gameTimer = function (time) {
   if (time >= 0) {
     setTimeout(function () {
       $('.timer').text(time);
       time -= 1;
       gameTimer(time);
     }, 1000);
+  } else {
+    $('#buttons button').attr("disabled", true);
   }
 };
 
